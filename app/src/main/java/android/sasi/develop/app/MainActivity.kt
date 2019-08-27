6ee741fefd6509ad39146ddd764dee7b5877d2f8
Sasikumar <sasisachin12@yahoo.in>
2019-08-27 23:20:16 +0530

    class added  for custom styles

--- app/src/main/java/android/sasi/develop/app/MainActivity.kt
+++ app/src/main/java/android/sasi/develop/app/MainActivity.kt
@@ -1,12 +1,15 @@
 package android.sasi.develop.app
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+       // styleButton.setGreenText(true)
+
     }
 }
